33c831106ce31b58d049ecee8fdbd5b6766c49a4
rasentaso <rasentaso@gmail.com>
2016-09-25 23:50:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ window.onload = function() {
     _back = new Back(0,0,displayWidth,displayHeight,stage);
 
     _board = new Board(stage);
-    _board.setUp(0,0,displayWidth / 2,9);
+    _board.setUp(0,0,displayWidth / 1.5,9);
     _board.refreshBoard();
 
 /*
