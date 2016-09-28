5106662c749d70dea1fe844f8c2775ed10803099
rasentaso <rasentaso@gmail.com>
2016-09-28 11:18:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -520,7 +520,7 @@ StoneStack.prototype.setUp = function(xpos,ypos,radius,color){
 
 StoneStack.prototype.refresh = function(){
     
-    this.lineStyle(2, ColorCode(this.color), 1);    
+    this.lineStyle(4, ColorCode(this.color), 1);    
     this.drawCircle(this.xpos, this.ypos, this.radius);    
     this.label.text = ' ' + this.count;
     this.endFill();
