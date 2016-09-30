38a655abc705772906ae3c75d50c655d188a9937
rasentaso <rasentaso@gmail.com>
2016-09-30 13:50:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -598,11 +598,16 @@ Guide.prototype.initialize = function(stage) {
 Guide.prototype.refresh = function(x,y){
     
     this.clear();   
+    _dtxt.text += 'refle ';
     if(_input_color !== 'blank'){
+    _dtxt.text += 'koko4';
+        
         this.beginFill(ColorCode(_input_color),0.5);
         this.drawCircle(x,y,_board.cell_half_length);            
         this.endFill();        
     }else if(_board.movingIds.length > 0){       
+    _dtxt.text += 'koko5 ';
+        
         var rootCellX  = _board.CellId2CellX(_board.movingIds[0]);
         var rootCellY  = _board.CellId2CellY(_board.movingIds[0]);
         for(var i = 0; i < _board.movingIds.length; ++i){
