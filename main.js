5a153a231eb5971ee18aa369fcb89ec4015a9f77
rasentaso <rasentaso@gmail.com>
2016-10-04 11:26:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -551,8 +551,8 @@ StoneStack.prototype.setUp = function(xpos,ypos,radius,color){
 
     this.xpos    = xpos;
     this.ypos    = ypos;
-    this.label.x = xpos - radius / 2;
-    this.label.y = ypos - radius / 2;
+//    this.label.x = xpos - radius / 2;
+//    this.label.y = ypos - radius / 2;
     this.radius  = radius;
     this.color   = color;
     if(color === 'white')this.reverse_color = 'black';
